f608d09400afcbdca742f8375f4247fc8ca32554
Ryashi <ryan.blade@hotmail.com>
2016-10-25 11:49:44 +0200

    added ENUM UserRIghts for future refacotring iterations

---
+++ baseProject/src/main/java/ch/unibe/ese/team8/model/UserRights.java
@@ -0,0 +1,12 @@
+package ch.unibe.ese.team8.model;
+
+/**
+ * NOTICE: not yet in use
+ *
+ * Enumarates all diffirent version of rights a user can have
+ *
+ * TODO: resolve old dependencies and use instead this enum.
+ */
+public enum UserRights {
+	USER, PREMIUM, ADMIN
+}
